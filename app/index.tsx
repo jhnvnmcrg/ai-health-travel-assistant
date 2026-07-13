6862bc6ff917ef1c576d2ae7bb0687c0770d797b
aybanpaie <johnivanmacaraeg01@gmail.com>
2026-07-13 11:21:09 +0800

    gluestack ui

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-black">
+      <Text className="text-2xl font-bold text-white">
+        AI Health Travel Assistant
+      </Text>
+    </SafeAreaView>
+  );
+}
